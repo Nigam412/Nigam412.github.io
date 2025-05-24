f6e950d04c3172511ad5e4d3bf5210d571f50a68
Nigam412 <nikshuforever@gmail.com>
2025-05-24 13:35:52 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("welcome to my page")
+const videos = document.querySelectorAll("video");
+
+videos.forEach(video => {
+    video.addEventListener("play", () => {
+        videos.forEach(v => {
+            if (v !== video) {
+                v.pause();
+            }
+        });
+    });
+});
+
+
+
+    
+    
+  
